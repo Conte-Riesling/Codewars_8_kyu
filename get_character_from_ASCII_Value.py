214a624652e4690408926e5196f854194c1b6a9f
Conte-Riesling <130985551+Conte-Riesling@users.noreply.github.com>
2023-06-22 15:44:54 +0300

    Add files via upload

---
+++ get_character_from_ASCII_Value.py
@@ -0,0 +1,8 @@
+# Write a function get_char() / getChar() which takes a number \n
+# and returns the corresponding ASCII char for that value.
+
+# Напишите функцию get_char() / getChar(), которая принимает число и \n
+# возвращает соответствующий символ ASCII для этого значения.
+
+def get_char(c):
+    return chr(c)
