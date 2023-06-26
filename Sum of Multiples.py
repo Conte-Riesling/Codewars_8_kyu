f74676a5fc2920be8607960ae377a1d786d0fe8b
Conte-Riesling <130985551+Conte-Riesling@users.noreply.github.com>
2023-06-26 16:59:55 +0300

    Add files via upload

---
+++ Sum of Multiples.py
@@ -0,0 +1,28 @@
+# Your Job: Find the sum of all multiples of n below m
+# Keep in Mind: n and m are natural numbers (positive integers)
+# m is excluded from the multiples
+
+# Твоя работа: Найдите сумму всех кратных n меньше m
+# Иметь ввиду: n и m - натуральные числа (целые положительные числа)
+# m исключается из кратных
+
+# First Solution:
+
+def sum_mul(n, m):
+    if n == 0 or m == 0:
+        return 'INVALID'
+    if n == m:
+        return n - m
+    if n < 0 or m < 0:
+        return 'INVALID'
+
+    my_list = [number for number in range(n, m) if number % n == 0]
+    return sum(my_list)
+
+# Second Solution:
+
+def sum_mul(n, m):
+    if m>0 and n>0:
+        return sum(range(n, m, n))
+    else:
+        return 'INVALID'
