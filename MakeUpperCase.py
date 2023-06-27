76935197798242b0ce72aa6d0fa7630c648357a1
Conte-Riesling <130985551+Conte-Riesling@users.noreply.github.com>
2023-06-27 15:19:55 +0300

    Add files via upload

---
+++ MakeUpperCase.py
@@ -0,0 +1,6 @@
+# Write a function which converts the input string to uppercase.
+
+# Напишите функцию, которая преобразует входную строку в верхний регистр.
+
+def make_upper_case(s):
+    return s.upper()
