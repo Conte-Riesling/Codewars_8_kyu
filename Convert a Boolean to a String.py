faff2095901935f51c4c32b90c3a888e3c522aa1
Conte-Riesling <130985551+Conte-Riesling@users.noreply.github.com>
2023-07-02 00:42:07 +0300

    Add files via upload

---
+++ Convert a Boolean to a String.py
@@ -0,0 +1,17 @@
+# Implement a function which convert the given boolean value into its string representation.
+# Note: Only valid inputs will be given.
+
+# Реализуйте функцию, которая преобразует данное логическое значение в его строковое представление.
+# Примечание. Будут даны только действительные входные данные.
+
+# First solution
+
+def boolean_to_string(b):
+    return 'True' if b else 'False'
+
+
+# Second solution
+
+def boolean_to_string(b):
+    return str(b)
+
