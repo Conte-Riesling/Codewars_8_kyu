93ae05f1381aff1eba1a97d2bdec820d68ccea73
Conte-Riesling <130985551+Conte-Riesling@users.noreply.github.com>
2023-07-03 00:26:23 +0300

    Add files via upload

---
+++ Capitalization and Mutability.py
@@ -0,0 +1,18 @@
+# Your coworker was supposed to write a simple helper function to capitalize a string \n
+# (that contains a single word) before they went on vacation.
+# Unfortunately, they have now left and the code they gave you doesn't work. \n
+# Fix the helper function they wrote so that it works as intended (i.e. make the first \n
+# character in the string "word" upper case). Don't worry about numbers, special characters, \n
+# or non-string types being passed to the function. The string lengths will be from 1 \n
+# character up to 10 characters, but will never be empty.
+
+# Ваш коллега должен был написать простую вспомогательную функцию, чтобы написать строку \n
+# (содержащую одно слово) с заглавной буквы перед тем, как уйти в отпуск.
+# К сожалению, они ушли, и код, который они вам дали, не работает. Исправьте вспомогательную \n
+# функцию, которую они написали, чтобы она работала как задумано (т. е. сделать первый символ \n
+# в строке «слово» прописным). Не беспокойтесь о числах, специальных символах или нестроковых \n
+# типах, которые передаются функции. Длина строки будет от 1 до 10 символов, но она никогда не \n
+# будет пустой.
+
+def capitalize_word(word):
+    return word.capitalize()
